67190e50ae804da25da7d22bc9b4cb6ba2d0b578
sundownspecial <je.mendoza@live.com>
2018-01-24 13:58:51 -0800

    set-up scaffold

---
+++ 08-merge-two-sll/lib/solution.js
@@ -0,0 +1,35 @@
+'use strict';
+
+const solution = module.exports = {};
+
+function round(value, decimals) {
+    return Number(Math.round(value+'e'+decimals)+'e-'+decimals);
+  }
+
+solution.twoLargest = function (array) {
+    if (array.length < 2) return null;
+    let allNumbers = true;
+    let  arrayInt = [];
+     for (let i = 0; i < array.length; i++) {
+      if(typeof array[i] !== 'number') {
+        allNumbers = false ;
+      }
+         arrayInt.push(round(array[i], 0))
+     }
+   
+   if (allNumbers == false) return null;
+ 
+   let arraySorted = arrayInt.sort(function(a, b){return b - a});
+   
+   //checks for duplicates
+   let arrayUnique = Array.from(new Set(arraySorted));
+    
+   return  { // object literal
+    largest: arrayUnique[0],
+    secondLargest: arrayUnique[1]
+    }
+ };
+
+
+
+
