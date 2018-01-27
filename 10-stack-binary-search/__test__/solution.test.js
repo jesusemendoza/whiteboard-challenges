b23bf66388d515ecca3d454a00b280bb9696a15c
sundownspecial <je.mendoza@live.com>
2018-01-26 23:04:04 -0800

    finished tests

--- 10-stack-binary-search/__test__/solution.test.js
+++ 10-stack-binary-search/__test__/solution.test.js
@@ -1,35 +1,47 @@
 'use strict';
 
 const solution = require('../lib/solution');
-const SLL = require('../lib/sll')
+const Stack = require('../lib/stack')
 require('jest');
+
+let testString = '{Spicy} jalapeno bacon ipsum dolor {amet spare} {{{ribs}}} ball tip drumstick, {venison} flank sausage shank alcatra. Chuck corned beef sirloin prosciutto jerky buffalo tongue short loin bresaola short ribs. Rump ribeye spare ribs t-bone leberkas cupim kielbasa. Strip steak prosciutto jerky short loin chicken chuck flank pastrami boudin alcatra porchetta pork cupim. Beef ribs salami ham drumstick leberkas alcatra.';
+let testStringInvalid1 = '{Spicy jalapeno bacon ipsum dolor {amet spare} {{{ribs}}} ball tip drumstick, {venison} flank sausage shank alcatra. Chuck corned beef sirloin prosciutto jerky buffalo tongue short loin bresaola short ribs. Rump ribeye spare ribs t-bone leberkas cupim kielbasa. Strip steak prosciutto jerky short loin chicken chuck flank pastrami boudin alcatra porchetta pork cupim. Beef ribs salami ham drumstick leberkas alcatra.';
+let testStringInvalid2 = 'Spicy} jalapeno bacon ipsum dolor {amet spare} {{{ribs}}} ball tip drumstick, {venison} flank sausage shank alcatra. Chuck corned beef sirloin prosciutto jerky buffalo tongue short loin bresaola short ribs. Rump ribeye spare ribs t-bone leberkas cupim kielbasa. Strip steak prosciutto jerky short loin chicken chuck flank pastrami boudin alcatra porchetta pork cupim. Beef ribs salami ham drumstick leberkas alcatra.';
+
 
 describe('Solution Module', function() {
   describe('#Two Largest', function() {
-    let sll = new SLL();
-     sll.insertHead(0);
-     sll.insertEnd(1);
-     sll.insertEnd(2);
-     sll.insertEnd(3);
-     sll.insertEnd(4);
-     sll.insertEnd(5);
-    let sllTest = sll.head.next.next
-    it('should take in a created SLL and return the correct value from the end', function() {
-      expect(solution.nthNode(sll, 2)).toEqual(sllTest);
-    });
 
     it('should take in a created SLL and return the correct value from the end', function() {
-      expect(solution.nthNode('sll', 2)).toEqual(null);
+      expect(solution.curlyString(testString)).toEqual('valid');
     });
-    
-    it('should take in a created SLL and return the correct value from the end', function() {
-      expect(solution.nthNode(sll, '2')).toEqual(null);
+    it('should take in an invalid string with an open bracket that doesnt have an close bracket', function() {
+      expect(solution.curlyString(testStringInvalid1)).toEqual('invalid string');
     });
-
-    it('should take in a created SLL and return the correct value from the end', function() {
-      expect(solution.nthNode(sll, 8)).toEqual('nth value from end is greater than length of list');
+    it('should take in an invalid string with a closed bracket that doesnt have an opening bracket', function() {
+      expect(solution.curlyString(testStringInvalid2)).toEqual('nothing in que to remove' );
     });
     
 
   });
-});
+    
+
+  describe('#Binary Search', function() {
+    let testArray = []
+    let kk = 10000000;
+    for (let i = 0; i < kk; i++) testArray.push(i);
+    
+    let sampleValidResult = {value: 22013, index: 22013}
+    it('should take in a valid input and return an object', function() {
+        expect(solution.binarySearch(testArray, 22013)).toEqual(`{value: ${22013}, index: ${22013}}`);
+    });
+
+    it('should return null when a string is put into the n parameter in the function', function() {
+      expect(solution.binarySearch(testArray, '22013')).toEqual(null);
+  });
+
+  it('should return null when an empty array is passed into the arr parameter in the function', function() {
+    expect(solution.binarySearch([], 22013)).toEqual(null);
+  });
+  });
+  });
--- 10-stack-binary-search/lib/solution.js
+++ 10-stack-binary-search/lib/solution.js
@@ -2,40 +2,38 @@
 
 const Stack = require('./stack')
 
-let testStack = new Stack();
-let testString = '{Spicy} jalapeno bacon ipsum dolor {amet spare} {{{ribs}}} ball tip drumstick, {venison} flank sausage shank alcatra. Chuck corned beef sirloin prosciutto jerky buffalo tongue short loin bresaola short ribs. Rump ribeye spare ribs t-bone leberkas cupim kielbasa. Strip steak prosciutto jerky short loin chicken chuck flank pastrami boudin alcatra porchetta pork cupim. Beef ribs salami ham drumstick leberkas alcatra.';
-
 const solution = module.exports = {};
 
-// solution.curlyString = function (str) {
-//   if (typeof str !== 'string') return null;
-//   for (let i = 0; i < str.length; i++) {
-//     if(str[i]=== '{') testStack.push('{');
-//     if(str[i]=== '}') {
-//     if(testStack.size === 0) return console.log('nothing in que to remove')  
-//     testStack.pop();
-//     }
-//   }
-//   console.log(testStack)
-//   if(testStack.top) return console.log('invalid string')
-//   return console.log('valid')
+solution.curlyString = function (str) {
+  let testStack = new Stack();
+  if (typeof str !== 'string') return null;
+  for (let i = 0; i < str.length; i++) {
+    if(str[i]=== '{') testStack.push('{');
+    if(str[i]=== '}') {
+    if(testStack.size === 0) return 'nothing in que to remove'  
+    testStack.pop();
+    }
+  }
+  console.log(testStack)
+  if(testStack.top) return 'invalid string';
+  return 'valid';
   
-// };
+};
 
 // solution.curlyString(testString);
 
-let testArray = [0,10,20,30,40,50,60,70,80,90,100]
+
 solution.binarySearch = function (arr, n) {
+  if (typeof n !== 'number' || typeof arr !== 'object' || arr.length === 0) return null;
   let index = Math.floor(arr.length/2);
   let max = arr.length
   let min = 0;
-  for (let k = 0; k<arr.length/(2); k++){
-    if (arr[index] === n) return console.log(`{value: ${arr[index]}, index: ${index}}`);
+  for (let k = 0; k<(arr.length/(k*1.24)); k++){
+    if (arr[index] === n) return `{value: ${arr[index]}, index: ${index}}`;
     if (arr[index] > n) max = index;      
     if (arr[index] < n) min = index;
     index = Math.floor((max+min)/2)
   }
-  return console.log('does not exist');
+  return 'does not exist';
 }
 
-solution.binarySearch(testArray,100);
