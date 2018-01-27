5590ff774a27fa31afe76eb372830d642a1671e0
sundownspecial <je.mendoza@live.com>
2018-01-26 19:27:21 -0800

    refactored code for binary search

--- 10-stack-binary-search/lib/solution.js
+++ 10-stack-binary-search/lib/solution.js
@@ -7,41 +7,35 @@ let testString = '{Spicy} jalapeno bacon ipsum dolor {amet spare} {{{ribs}}} bal
 
 const solution = module.exports = {};
 
-solution.curlyString = function (str) {
-  if (typeof str !== 'string') return null;
-  for (let i = 0; i < str.length; i++) {
-    if(str[i]=== '{') testStack.push('{');
-    if(str[i]=== '}') {
-    if(testStack.size === 0) return console.log('nothing in que to remove')  
-    testStack.pop();
-    }
-  }
-  console.log(testStack)
-  if(testStack.top) return console.log('invalid string')
-  return console.log('valid')
-  
-};
-
-solution.curlyString(testString);
-
-// let testArray = [0,10,20,30,40,50,60,70,80,90]
-// solution.binarySearch = function (arr, n) {
-//   let location = Math.floor(arr.length/2);
-//   let max = arr.length
-//   let min = 0;
-//   let range = max-min;
-//   for (let k = 0; k<arr.length; k++){
-//     console.log('itr: ', k)
-//     console.log('range: ',range)
-//     console.log('value: ',arr[location])
-//     if (arr[location] > n)  max = location;
-//     if (arr[location] < n)  min = location;
-//     console.log('max: ',max, 'min: ',min)
-//     if (arr[location] === n) return console.log(`{value: ${arr[location]}, index: ${location}}`);
-//     range = max-min;
-//     location = Math.floor(range/2)
+// solution.curlyString = function (str) {
+//   if (typeof str !== 'string') return null;
+//   for (let i = 0; i < str.length; i++) {
+//     if(str[i]=== '{') testStack.push('{');
+//     if(str[i]=== '}') {
+//     if(testStack.size === 0) return console.log('nothing in que to remove')  
+//     testStack.pop();
+//     }
 //   }
-//   return console.log('does not exist');
-// }
+//   console.log(testStack)
+//   if(testStack.top) return console.log('invalid string')
+//   return console.log('valid')
+  
+// };
 
-// solution.binarySearch(testArray,10);
+// solution.curlyString(testString);
+
+let testArray = [0,10,20,30,40,50,60,70,80,90,100]
+solution.binarySearch = function (arr, n) {
+  let index = Math.floor(arr.length/2);
+  let max = arr.length
+  let min = 0;
+  for (let k = 0; k<arr.length/(2); k++){
+    if (arr[index] === n) return console.log(`{value: ${arr[index]}, index: ${index}}`);
+    if (arr[index] > n) max = index;      
+    if (arr[index] < n) min = index;
+    index = Math.floor((max+min)/2)
+  }
+  return console.log('does not exist');
+}
+
+solution.binarySearch(testArray,100);
