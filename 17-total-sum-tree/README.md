1cfb3154591e90bf6265f27686c847c1d84363e0
sundownspecial <je.mendoza@live.com>
2018-02-06 19:16:40 -0800

    added picture and readme

--- 17-total-sum-tree/README.md
+++ 17-total-sum-tree/README.md
@@ -1,2 +1,4 @@
-Whiteboard Challenge 09
-Write a function which accepts n and a linked list as it's arguments, and will return the nth from last node in a linked list
+Whiteboard Challenge 17
+Complete today's whiteboard challenge and follow the submission instructions below:
+
+Write a function that accepts a Tree and returns the total sum of all the elements in the tree. - You are to assume that each node will have a val property which has a numeric value. - Any other data points, aside from children are irrelevant.
--- 17-total-sum-tree/__test__/solution.test.js
+++ 17-total-sum-tree/__test__/solution.test.js
@@ -1,35 +1,38 @@
 'use strict';
 
 const solution = require('../lib/solution');
-const SLL = require('../lib/sll')
+const K = require('../lib/k-ary');
 require('jest');
 
 describe('Solution Module', function() {
-  describe('#Two Largest', function() {
-    let sll = new SLL();
-     sll.insertHead(0);
-     sll.insertEnd(1);
-     sll.insertEnd(2);
-     sll.insertEnd(3);
-     sll.insertEnd(4);
-     sll.insertEnd(5);
-    let sllTest = sll.head.next.next
-    it('should take in a created SLL and return the correct value from the end', function() {
-      expect(solution.nthNode(sll, 2)).toEqual(sllTest);
-    });
+  describe('#Total Sum', function() {
+    let k = new K();
+    k.insert(0);
+    k.insert(1,0);
+    k.insert(2,1);
+    k.insert(3,2);
+    k.insert(20,0);
+    k.insert(14,0);
 
-    it('should take in a created SLL and return the correct value from the end', function() {
-      expect(solution.nthNode('sll', 2)).toEqual(null);
+    
+    it('should take in a created K-ary Tree and return the correct solution', function() {
+      expect(solution.totalSum(k)).toEqual(40);
     });
     
-    it('should take in a created SLL and return the correct value from the end', function() {
-      expect(solution.nthNode(sll, '2')).toEqual(null);
-    });
-
-    it('should take in a created SLL and return the correct value from the end', function() {
-      expect(solution.nthNode(sll, 8)).toEqual('nth value from end is greater than length of list');
+    let j = new K();
+    j.insert(0);
+    j.insert(1,0);
+    j.insert(2,1);
+    j.insert(3,2);
+    j.insert(20,0);
+    j.insert(14,0);
+    j.insert('14',0);
+    it('should take in a created k-ary tree with one value as text and ignore non-number values', function() {
+      expect(solution.totalSum(j)).toEqual(40);
     });
     
-
+    it('should take in a string and reject it as an invalid input', function() {
+      expect(solution.totalSum('5')).toEqual('Invalid input');
+    });
   });
 });
