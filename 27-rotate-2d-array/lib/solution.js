beb076dfaab00bbf82213bea081d625a54fd9a97
sundownspecial <je.mendoza@live.com>
2018-02-20 15:50:26 -0800

    added whitboard

--- 27-rotate-2d-array/lib/solution.js
+++ 27-rotate-2d-array/lib/solution.js
@@ -5,12 +5,16 @@ solution.rotate = function (arr) {
   if(typeof arr !== 'object') return 'input not an array';
   if(arr.length === 0) return 'invalid array';
   let finalArr = [];
+  let num=0;
   for(let i = arr.length-1; i >= 0; i--){
+    num++;
     let tempArr = [];
     for(let k = 0; k <arr.length;k++){
+      num++;
       tempArr.push(arr[k][i]);
     }
     finalArr.push(tempArr);
   }
+  console.log(num);
   return finalArr;
 };
